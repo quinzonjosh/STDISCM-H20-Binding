2063934b70decebcea0808d22b78ea82b2005c0c
cymonyy <cymon_nadela@dlsu.edu.ph>
2024-03-08 12:56:15 +0800

    basic implementation of hydrogen

--- src/Hydrogen.java
+++ src/Hydrogen.java
@@ -1,2 +1,25 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Hydrogen{
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter N Requests: ");
+        long numRequests = scanner.nextLong();
+
+        for (long i = 1; i <= numRequests; i++) {
+            System.out.printf(
+                    "H%d, request, %s%n",
+                    i,
+                    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+                            .format(new Date())
+            );
+        }
+
+    }
+
 }
