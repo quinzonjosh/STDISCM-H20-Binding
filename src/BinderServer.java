1091a0f68e4192fa7d9486c3e815ed499d5565ff
Josh Quinzon <quinzonjosh261@gmail.com>
2024-03-29 20:16:54 +0800

    implemented Hydrogen & Binder TCP w/o batch sending

--- src/BinderServer.java
+++ src/BinderServer.java
@@ -1,3 +1,5 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -6,13 +8,66 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class BinderServer {
-    private static final int BINDER_SERVER_PORT = 4999;
-    BlockingQueue<String> hydrogenQueue = new LinkedBlockingQueue<>();
-    BlockingQueue<String> oxygenQueue = new LinkedBlockingQueue<>();
+    private ServerSocket serverSocket;
+    private DataInputStream dis;
+    private DataOutputStream dos;
+    private BlockingQueue<String> hydrogenQueue = new LinkedBlockingQueue<>();
+
+    public BinderServer(int port){
+        try {
+            this.serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void start() {
+        System.out.println("Server is running & listening for connections...");
+        while(true){
+            try {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client " + clientSocket + " connected");
+
+                setupInputAndOutputStreams(clientSocket);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setupInputAndOutputStreams(Socket clientSocket) {
+        try {
+            this.dis = new DataInputStream(clientSocket.getInputStream());
+//            this.dos = new DataOutputStream(clientSocket.getOutputStream());
+
+            getDataFromClients();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void getDataFromClients() {
+        try {
+            int hydrogenCount = dis.readInt();
+            for (int i=0; i<hydrogenCount; i++){
+                hydrogenQueue.offer(dis.readUTF());
+            }
+
+            sanityCheck();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void sanityCheck() {
+        System.out.println("hydrogen queue: " + hydrogenQueue);
+
+    }
 
     public static void main(String[] args) {
-
-
+        int SERVER_PORT = 4999;
+        BinderServer binderServer = new BinderServer(SERVER_PORT);
+        binderServer.start();
     }
 
 }
