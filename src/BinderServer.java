b3a7b50dc4e4a0e93230d899d54ee7aae19b324a
eerah <70436107+iraholliva@users.noreply.github.com>
2024-04-02 02:09:50 +0800

    boogsh TODO

---
+++ src/BinderServer.java
@@ -0,0 +1,183 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class BinderServer {
+    private ServerSocket serverSocket;
+    private BlockingQueue<Element> hydrogenQueue = new LinkedBlockingQueue<>();
+    private BlockingQueue<Element> oxygenQueue = new LinkedBlockingQueue<>();
+
+    private static final int MAX_HYDROGEN_TO_BOND = 2;
+
+    private static final int MAX_OXYGEN_TO_BOND = 1;
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-M-d HH:mm:ss.SSS");
+
+    /*
+    * @TODO: Implement Close Program after finish Binding (all requests must push thru) (BinderServer)
+    * @TODO: Sanity Check - Print leftovers (Hydrogen/Oxygen)
+    * @TODO: Sanity Check - Check if molecule is requested first after binding (Hydrogen/Oxygen)
+    * @TODO: Check Errors from TODO 3
+    * */
+
+
+
+
+    public static void main(String[] args) {
+        int SERVER_PORT = 4999;
+        BinderServer binderServer = new BinderServer(SERVER_PORT);
+        binderServer.start();
+    }
+
+    public BinderServer(int port){
+        try {
+            this.serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void start() {
+        System.out.println("Server is running & listening for connections...");
+
+
+        Thread bindingThread = new Thread(new BindingHandler(hydrogenQueue, oxygenQueue));
+        bindingThread.start();
+
+        while(true){
+            try {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client " + clientSocket + " connected");
+                Thread clientHandler = new Thread(new ClientHandler(clientSocket));
+                clientHandler.start();
+            } catch (IOException e){
+                e.printStackTrace();
+                break;
+            }
+        }
+
+
+
+    }
+
+    private void sanityCheck() {
+        List<String> list = new ArrayList<>();
+        hydrogenQueue.forEach(element -> list.add(element.getElement()));
+        System.out.println("hydrogen queue: " + Arrays.toString(list.toArray()));
+
+        list.clear();
+        oxygenQueue.forEach(element -> list.add(element.getElement()));
+        System.out.println("Oxygen queue: " + Arrays.toString(list.toArray()));
+    }
+
+    private static class BindingHandler implements Runnable {
+
+        private BlockingQueue<Element> hydrogenQueue;
+        private BlockingQueue<Element> oxygenQueue;
+
+        public BindingHandler(BlockingQueue<Element> hydrogenQueue, BlockingQueue<Element> oxygenQueue) {
+            this.hydrogenQueue = hydrogenQueue;
+            this.oxygenQueue = oxygenQueue;
+        }
+
+        @Override
+        public void run() {
+            while (true){
+                if (hydrogenQueue.size() >= MAX_HYDROGEN_TO_BOND && oxygenQueue.size() >= MAX_OXYGEN_TO_BOND){
+
+                    List<Element> elements = new ArrayList<>();
+                    elements.add(hydrogenQueue.poll());
+                    elements.add(hydrogenQueue.poll());
+                    elements.add(oxygenQueue.poll());
+
+                    bond(elements);
+                }
+            }
+        }
+
+        private void bond(List<Element> elements) {
+            elements.forEach(element -> {
+                try {
+                    DataOutputStream dos = new DataOutputStream(element.getClientSocket().getOutputStream());
+                    String log = element.getElement() + ", bonded, " + LocalDateTime.now().format(FORMATTER);
+                    System.out.println("Server: " + log);
+                    dos.writeUTF(log);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            });
+        }
+
+    }
+    private class ClientHandler implements Runnable{
+        private Socket clientSocket;
+        private DataInputStream dis;
+
+        public ClientHandler(Socket clientSocket){
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run(){
+            try {
+                this.dis = new DataInputStream(clientSocket.getInputStream());
+
+                String clientType = dis.readUTF();
+
+                switch (clientType) {
+                    case "Hydrogen":
+                        handleHydrogenClient();
+                        break;
+                    case "Oxygen":
+                        handleOxygenClient();
+                        break;
+                    default:
+                        System.out.println("Unknown client type");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            finally {
+//                try {
+//                    clientSocket.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+        }
+
+        private void handleOxygenClient() throws IOException {
+            while (true) {
+                String molecule = dis.readUTF();
+                if (molecule.equals("DONE")) {
+                    break;
+                } else {
+                    oxygenQueue.offer(new Element(clientSocket, molecule));
+                }
+            }
+            sanityCheck();
+        }
+
+        private void handleHydrogenClient() throws IOException {
+            while (true) {
+                String molecule = dis.readUTF();
+                if (molecule.equals("DONE")) {
+                    break;
+                } else {
+                    hydrogenQueue.offer(new Element(clientSocket, molecule));
+                }
+            }
+            sanityCheck();
+        }
+    }
+
+
+}
---
+++ src/HydrogenClient.java
@@ -0,0 +1,178 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class HydrogenClient {
+    public static int hydrogenCount;
+    private DataInputStream dis;
+    private DataOutputStream dos;
+    private int SERVER_PORT;
+    private String SERVER_ADDRESS;
+    public static final int NTHREADS = 8;
+
+    private final BlockingQueue<Interval> intervals = new LinkedBlockingQueue<>();
+
+    private List<Thread> threads = new ArrayList<>();
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-M-d HH:mm:ss.SSS");
+    public HydrogenClient(String SERVER_ADDRESS, int SERVER_PORT){
+        this.SERVER_ADDRESS = SERVER_ADDRESS;
+        this.SERVER_PORT = SERVER_PORT;
+    }
+
+    private void start() {
+        try{
+            Socket serverSocket = new Socket(this.SERVER_ADDRESS, this.SERVER_PORT);
+            System.out.println("Connected to server.");
+
+            this.dos = new DataOutputStream(serverSocket.getOutputStream());
+            this.dis = new DataInputStream(serverSocket.getInputStream());
+
+            Thread serverListener = new Thread(new ServerMessageReceiver(dis));
+            serverListener.start();
+
+
+            getUserInput();
+            sendHydrogenMolecules();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void sendHydrogenMolecules() {
+        int batch = hydrogenCount / NTHREADS;
+
+
+        for (int i = 0; i < NTHREADS; i++) {
+            Thread thread = new Thread(new DataSender(intervals));
+            threads.add(thread);
+            thread.start();
+        }
+
+        try {
+            dos.writeUTF("Hydrogen");
+            for (int i = 0; i < NTHREADS; i++) {
+                final int start = i * batch;
+                final int end = (i == NTHREADS - 1) ? hydrogenCount : (i + 1) * batch;
+
+                synchronized (intervals) {
+                    intervals.offer(new Interval(start, end, false));
+                    intervals.notifyAll();
+                }
+            }
+
+            synchronized (intervals) {
+                intervals.offer(new Interval(-1, -1, true));
+                intervals.notifyAll();
+            }
+
+            for (Thread thread : threads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    // Handle InterruptedException
+                    e.printStackTrace();
+                }
+            }
+            dos.writeUTF("DONE");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private class DataSender implements Runnable{
+
+        private final BlockingQueue<Interval> intervals;
+
+        public DataSender(BlockingQueue<Interval> intervals) {
+            this.intervals = intervals;
+        }
+
+        @Override
+        public void run() {
+            synchronized (intervals) {
+                while (true) {
+                    while (intervals.isEmpty()) {
+                        try {
+                            intervals.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                    Interval interval = intervals.poll();
+                    assert interval != null;
+
+                    if (interval.isLast()) {
+                        synchronized (intervals) {
+                            intervals.offer(interval);
+                            intervals.notifyAll();
+                        }
+                        break;
+                    }
+
+                    for(int j = interval.getStart(); j < interval.getEnd(); j++){
+                        try {
+                            String element = "H"+j;
+                            dos.writeUTF(element);
+//                            dos.flush();
+                            String log = element + ", requested, " + LocalDateTime.now().format(FORMATTER);
+                            System.out.println(log);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private static class ServerMessageReceiver implements Runnable{
+
+        private DataInputStream dis;
+
+        public ServerMessageReceiver(DataInputStream dis) {
+            this.dis = dis;
+        }
+
+        @Override
+        public void run() {
+            while (true){
+                try {
+                    String log = dis.readUTF();
+                    System.out.println("From Server: " + log);
+
+                } catch (IOException e){
+                    e.printStackTrace();
+                    break;
+                }
+            }
+
+        }
+    }
+
+    private static void getUserInput() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of Hydrogen Molecules to send: ");
+        hydrogenCount = input.nextInt();
+    }
+
+    public static void main(String[] args) {
+        int SERVER_PORT = 4999;
+        String SERVER_ADDRESS = "localhost";
+
+        HydrogenClient hydrogenClient = new HydrogenClient(SERVER_ADDRESS, SERVER_PORT);
+        hydrogenClient.start();
+    }
+}
